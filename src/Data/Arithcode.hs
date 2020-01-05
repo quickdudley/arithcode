b04565499bbaa1e2f746f12fd3f126634a0ba780
Jeremy List <quick.dudley@gmail.com>
2020-01-05 16:27:42 +1300

    Refactor and prevent requesting unneeded entropy.

--- src/Data/Arithcode.hs
+++ src/Data/Arithcode.hs
@@ -80,15 +80,13 @@ exhausted (Interval a b) = a <= 0 && b >= 1
 
 data RangeTree a = Leaf a | Node Rational (RangeTree a) (RangeTree a)
 
-reduceTree :: Bool -> Interval -> RangeTree a -> (Interval, RangeTree a)
-reduceTree _ i l@(Leaf _) = (i,l)
-reduceTree e i n@(Node m l r) = let
-  leftBranch = reduceTree e (widen (Interval 0 m) i) l
-  rightBranch = reduceTree e (widen (Interval m 1) i) r
+reduceTree :: Interval -> RangeTree a -> (Interval, RangeTree a)
+reduceTree i l@(Leaf _) = (i,l)
+reduceTree i n@(Node m l r) = let
+  leftBranch = reduceTree (widen (Interval 0 m) i) l
+  rightBranch = reduceTree (widen (Interval m 1) i) r
   in case (overlap i (Interval 0 m), overlap i (Interval m 1)) of
-    (True,True)
-      | e -> leftBranch
-      | otherwise -> (i,n)
+    (True,True) -> (i,n)
     (True,False) -> leftBranch
     (False,True) -> rightBranch
     (False,False) -> error "Interval is outside \"0...1\""
@@ -96,7 +94,7 @@ reduceTree e i n@(Node m l r) = let
 data Step m a =
   R a |
   A (m (Step m a)) |
-  I (Interval -> Bool -> (Interval,Step m a))
+  I (Maybe Interval -> Step m a)
 
 newtype ArithcodeT m a = ACT (forall b . (a -> Step m b) -> Step m b)
 
@@ -116,33 +114,30 @@ instance Monad (ArithcodeT m) where
 instance MonadTrans ArithcodeT where
    lift a = ACT (A . (<$> a))
 
+step :: Functor m => Step m a -> Interval -> Step m a
+step s@(R _) _ = s
+step (A a) i = A $ fmap (flip step i) a
+step (I s) i = s (Just i)
+
+starve :: Functor m => Step m a -> Step m a
+starve s@(R _) = s
+starve (A a) = A $ fmap starve a
+starve (I s) = s Nothing
+
 runArithcodeT :: Monad m => ArithcodeT m a -> [Interval] -> m a
 runArithcodeT (ACT z) = go (z R) where
   go (R a) _ = return a
   go (A a) l = a >>= flip go l
-  go s@(I _) [] = go s [Interval 0 1]
-  go (I c) (i:r) = let
-    (i',n) = c i (null r)
-    l = if wantNext i'
-      then case r of
-        [] -> [i']
-        (i2:z) -> narrow i' i2 : z
-      else i' : r
-    in go n l
+  go (I s) [] = go (s Nothing) []
+  go (I c) (i:r) = go (c $ Just i) r
 
 runArithcodeT' :: Monad m => ArithcodeT m a -> m (Maybe Interval) -> m a
-runArithcodeT' (ACT z) s = go False (z R) (Interval 0 1) where
-  go e (R a) _ = return a
-  go e (A a) i = a >>= flip (go e) i
-  go e t@(I c) i = let
-   (i',n) = c i e
-   in if wantNext i' && not e
-     then s >>= \mi -> case mi of
-       Nothing -> go True n i'
-       Just i2 -> go e n (narrow i' i2)
-     else go e n i'
+runArithcodeT' (ACT z) src = go (z R) where
+  go (R a) = return a
+  go (A a) = a >>= go
+  go (I s) = src >>= (go . s)
 
-element :: [(Rational,a)] -> ArithcodeT m a
+element :: Functor m => [(Rational,a)] -> ArithcodeT m a
 element [] = error "\'element\' requires non-empty list"
 element l = let
   mergePairs [] = []
@@ -156,66 +151,72 @@ element l = let
     map (\(p,v) -> (Leaf v,p)) $
     filter (\(p,_) -> p > 0) l
   in ACT (\c -> let
-    go e i n = case reduceTree e i n of
+    fin (Leaf a) _ = starve $ c a
+    fin (Node m l r) i@(Interval a _) = if m < a
+      then fin l (widen i (Interval 0 m))
+      else fin r (widen i (Interval m 1))
+    go i n = case reduceTree i n of
       (Interval 0 1, Leaf a) -> c a
-      (i1, Leaf a) -> I (\i2 _ -> (narrow i1 i2, c a))
-      (i1, n@(Node _ _ _)) -> I (\i2 e' ->
-        (Interval 0 1, go e' (narrow i1 i2) n)
+      (i1, Leaf a) -> step (c a) i1
+      (i1, n@(Node _ _ _)) -> I (\mi2 -> case mi2 of
+        Just i2 -> go (narrow i1 i2) n
+        Nothing -> starve $ step (fin n i1) i1
        )
-    in go False (Interval 0 1) tree
+    in go (Interval 0 1) tree
    )
 
-integralInRange :: Integral n => n -> n -> ArithcodeT m n
+integralInRange :: (Functor m,Integral n) => n -> n -> ArithcodeT m n
 integralInRange l h = ACT (\c -> let
-  go e a b = let
+  go a b = let
     [a',b'] = map floor [a,b] :: [Integer]
-    in if a' == b' || e
+    in if a' == b'
       then let
         r = c (fromIntegral a')
         in case map (subtract (fromIntegral a')) [a,b] of
           [0,1] -> r
-          [ar,br] -> I (\i' _ -> (narrow (Interval ar br) i',r))
-      else I (\i' e' -> let
-        Interval ar br = narrow (Interval a b) i'
-        in (Interval 0 1, go e' ar br)
+          [ar,br] -> step r (Interval ar br)
+      else I (\mi' -> case mi' of
+        Just i' -> let
+          Interval ar br = narrow (Interval a b) i'
+          in go ar br
+        Nothing -> starve $ c $ fromIntegral a'
        )
-  in go False (fromIntegral l) ((fromIntegral h) + 1)
+  in go (fromIntegral l) ((fromIntegral h) + 1)
  )
 
-encodeIntegral :: Integral n => n -> n -> ArithcodeT m n
+encodeIntegral :: (Functor m,Integral n) => n -> n -> ArithcodeT m n
 encodeIntegral l h = ACT (\c -> let
-  go e a b = let
-    [a0,b'] = map floor [a,b] :: [Integer]
-    in if a0 == b' || e
+  go a b = let
+    [a',b'] = map floor [a,b] :: [Integer]
+    in if a' == b'
       then let
-        (a',e') = if e && a0 /= b'
-          then (ceiling a,True)
-          else (a0,False)
         r = c (fromIntegral a')
         in case map (subtract (fromIntegral a')) [a,b] of
-          _ | e' -> r
           [0,1] -> r
-          [ar,br] -> I (\i' _ -> (narrow (Interval ar br) i',r))
-      else I (\i' e' -> let
-        Interval ar br = narrow (Interval a b) i'
-        in (Interval 0 1, go e' ar br)
+          [ar,br] -> step r (Interval ar br)
+      else I (\mi' -> case mi' of
+        Just i' -> let
+          Interval ar br = narrow (Interval a b) i'
+          in go ar br
+        Nothing -> starve $ c $ ceiling a
        )
-  in go False (fromIntegral l) ((fromIntegral h) + 1)
+  in go (fromIntegral l) ((fromIntegral h) + 1)
  )
 
-eof :: ArithcodeT m Bool
-eof = ACT (\c ->
-  I (\i e -> (i,c (e && wantNext i)))
- )
+eof :: Functor m => ArithcodeT m Bool
+eof = ACT (\c -> I (\m -> case m of
+  Nothing -> starve $ c True
+  Just i -> step (c False) i
+ ))
 
-exhaust :: ArithcodeT m a -> ArithcodeT m [a]
+exhaust :: Functor m => ArithcodeT m a -> ArithcodeT m [a]
 exhaust d = go where
   go = eof >>= \e -> if e
     then return []
     else d >>= \a -> go >>= \r -> return (a:r)
 
 -- | Version of 'exhaust' which is tail-recursive in strict monads
-exhaust' :: ArithcodeT m a -> ArithcodeT m [a]
+exhaust' :: Functor m => ArithcodeT m a -> ArithcodeT m [a]
 exhaust' d = go id where
   go acc = eof >>= \e -> if e
     then return (acc [])
